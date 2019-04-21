b2971488cfc3cbe3119d70c66dfa373a6010168d
ismail ÇADIR <ismailcadir@gmail.com>
2019-04-21 11:46:00 +0300

    Identity Kullanımı. Role Olusturma ve Authorize örnek

---
+++ Abc.MvcWebUI/Identity/ApplicationRole.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Abc.MvcWebUI.Identity
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public string Description { get; set; }
+
+        public ApplicationRole()
+        {
+
+        }
+
+        public ApplicationRole(string rolename, string description)
+        {
+            this.Description = description;
+        }
+    }
+}
---
+++ Abc.MvcWebUI/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Abc.MvcWebUI.Models
+{
+    public class Login
+    {
+        [Required]
+        [DisplayName("Kullanıcı Adı")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DisplayName("Şifre")]
+        public string Password { get; set; }
+
+
+        [DisplayName("Beni Hatırla")]
+        public bool RememberMe { get; set; }
+    }
+}
